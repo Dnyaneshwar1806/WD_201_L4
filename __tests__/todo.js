52afc61d69d4b6726c415451a9c6d82a532e1656
Dnyaneshwar Kolape <113690251+Dnyaneshwar1806@users.noreply.github.com>
2022-11-17 02:25:57 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,35 +1,44 @@
 const todoList = require("../todo");
 const { all, add, markAsComplete, overdue, dueLater, dueToday } = todoList();
 
-describe("TodoList Test Suite", () => {
+describe("Todolist test Suite", () => {
+   
   beforeAll(() => {
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
+    add(
+      {
+        title: "test todo",
+        completed: false,
+        dueDate: new Date().toLocaleDateString("en-CA")
+      }
+    );
+  })
+   test("Should add new todo", () => {
+     const todoItemsCount = all.length;
+    add(
+      {
+        title: "test todo",
+        completed: false,
+        dueDate: new Date().toLocaleDateString("en-CA")
+      }
+    );
+    expect(all.length).toBe(todoItemsCount + 1 );
   });
-  test("Should add new todo", () => {
-    const todoItemsCount = all.length;
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-    expect(all.length).toBe(todoItemsCount + 1);
-  });
-  test("Should mark todo as completed", () => {
+  
+  test("Should mark a to do as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
-  test("Should ckeck retrieval of overdue items", () => {
-    expect(overdue().length).toBe(0);
+  
+  test("Should check over due items", () => {
+    expect(overdue().length).toBe(1);
   });
-  test("Should ckeck retrieval of dueToday items", () => {
+  
+  test("Should check due Today items ", () => {
     expect(dueToday().length).toBe(2);
   });
-  test("Should ckeck retrieval of dueLater items", () => {
-    expect(dueLater().length).toBe(0);
+  
+  test("Should check due later items", () => {
+    expect(dueLater().length).toBe(2);
   });
-});
+}) 
