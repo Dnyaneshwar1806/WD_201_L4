46cbecd2cf90acb80d514e14e50065d175bb84e0
Dnyaneshwar Kolape <113690251+Dnyaneshwar1806@users.noreply.github.com>
2022-11-19 23:16:46 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,46 +1,37 @@
 const todoList = () => {
-    all = []
+    all = [];
     const add = (todoItem) => {
-      all.push(todoItem)
-    }
+      all.push(todoItem);
+    };
     const markAsComplete = (index) => {
-      all[index].completed = true
-    }
-    
+      all[index].completed = true;
+    };
+  
     const overdue = () => {
-      return all.filter((item)=> {
-          return item.completed === false && item.dueDate <= new Date().toLocaleDateString("en-CA")
-      })
-    }
+      return all.filter(
+        (todo) => todo.dueDate < new Date().toLocaleDateString("en-CA")
+      );
+    };
+  
     const dueToday = () => {
-      return all.filter((item)=> {
-        return item.dueDate === new Date().toLocaleDateString("en-CA") 
-      })
-    }
+      return all.filter(
+        (todo) => todo.dueDate === new Date().toLocaleDateString("en-CA")
+      );
+    };
+  
     const dueLater = () => {
-      return all.filter((item)=> {
-        return item.dueDate >= new Date().toLocaleDateString("en-CA") 
-      })
-    }
-    const toDisplayableList = (list) => {
-      let tasklist = [];
-      list.forEach((item) => {
-        if (item.dueDate === today) {
-          if (item.completed === true) {
-            tasklist.push(`[x] ${item.title}`)
-          } else {
-            tasklist.push(`[ ] ${item.title}`)
-          }
-        }else{
-          if (item.completed === true) {
-            tasklist.push(`[x] ${item.title} ${item.dueDate}`)
-          }else{
-            tasklist.push(`[ ] ${item.title} ${item.dueDate}`)
-          }
-        }
-      })
-      return tasklist.join("\n")
-    }
-    return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
-  }
-module.exports = todoList;
+      return all.filter(
+        (todo) => todo.dueDate > new Date().toLocaleDateString("en-CA")
+      );
+    };
+    return {
+      all,
+      add,
+      markAsComplete,
+      overdue,
+      dueToday,
+      dueLater,
+    };
+  };
+  
+  module.exports = todoList;
